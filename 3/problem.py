00f362faee96a3cff6981dea65b3193c0312fca3
= <jeremy.turinetti@gmail.com>
2024-12-02 23:21:59 -0600

    day 3 problems 1 and 2

--- 3/problem.py
+++ 3/problem.py
@@ -1,16 +1,32 @@
 import time
 import logging
+import re
 from utils import *
 from my_utils import read_aoc_data
 
+def algo(input, regex):
+    sum = 0
+    do = True
+    for s in input:
+        results = re.findall(regex, s)
+        for match in results:
+            if match == "do()":
+                do = True
+                continue
+            if match == "don't()":
+                do = False
+                continue
+            if do:
+                sm = match.split(',')
+                sum = sum + int(sm[0].split('(')[1]) * int(str(sm[1][:-1]))
+    return sum
+
 # solution functions
 def part_a(input):
-    # TODO
-    return
+    return algo(input, r"mul\([0-9]+,[0-9]+\)")
 
 def part_b(input):
-    # TODO
-    return
+    return algo(input, r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)")
 
 def execute():
     input_data = read_aoc_data(3, 2024)    # replace with correct day and year
