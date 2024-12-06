62808a10b55e5d29b118209b39a21c75c03b5a6d
= <jeremy.turinetti@gmail.com>
2024-12-06 00:04:03 -0600

    day 6 part 2 WIP

--- 6/problem.py
+++ 6/problem.py
@@ -2,6 +2,7 @@ import time
 import logging
 from utils import *
 from my_utils import *
+import copy
 
 def turn_right(current_direction):
     if current_direction == '^':
@@ -19,7 +20,8 @@ def traverse_position(grid, coordinate):
     grid[coordinate[0]] = "".join(temp)
 
 # solution functions
-def part_a(input):
+def part_a(orig_input):
+    input = copy.deepcopy(orig_input)
     rows = len(input)
     cols = len(input[0])
     current = (0,0)
@@ -64,9 +66,55 @@ def part_a(input):
 
     return counter
 
-def part_b(input):
-    # TODO
-    return
+def part_b(orig_input):
+    input = copy.deepcopy(orig_input)
+    rows = len(input)
+    rows = len(input)
+    cols = len(input[0])
+    current = (0,0)
+    for x in range(rows - 1):
+        if input[x].find('^') > -1:
+            current = (x, input[x].find('^'))
+            break
+    
+    # start traversing!
+    current_direction = '^'
+    inside_grid = True
+    counter = 1
+    loop_counter = 0
+    traverse_position(input, current)
+    while (inside_grid):
+        next = ()
+        if current_direction == '^':
+            # go up
+            next = up_one(current)            
+        elif current_direction == '>':
+            # go right
+            next = right_one(current, cols)            
+        elif current_direction == '<':
+            # go left
+            next = left_one_unsafe(current)
+        elif current_direction == 'v':
+            # go down
+            next = down_one(current, rows)        
+        
+        if (next[0] < 0 or next[0] >= rows or next[1] < 0 or next[1] >= cols):
+            print('done!')
+            inside_grid = False
+        elif input[next[0]][next[1]] == '#':
+            # turn right
+            current_direction = turn_right(current_direction)
+            print('turning right; new direction is {}'.format(current_direction))
+        else:
+            print('moving {} from {} to {}'.format(current_direction, current, next))
+            current = next
+            if (input[current[0]][current[1]] != 'X'):
+                counter = counter + 1
+                traverse_position(input, current)
+            else:
+                loop_counter = loop_counter + 1
+
+    return loop_counter
 
 def execute():
     input_data = read_aoc_data(6, 2024)    # replace with correct day and year
