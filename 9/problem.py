9939e2893a51e88c0d2313ab9c270352a4fc68b0
= <jeremy.turinetti@gmail.com>
2024-12-09 22:24:33 -0600

    day 9 part 1...I am not proud of this

--- 9/problem.py
+++ 9/problem.py
@@ -17,40 +17,46 @@ def part_a(input):
         else:
             # free space
             block_map = block_map + '.' * cur_digit
+    
     print(block_map)
 
+    checksum = 0
     cur_id = cur_id - 1
 
     checksum_index = 0
     reverse_index = len(input[0]) - 1
     block_map_reverse_index = len(block_map) - 1
-    checksum = 0
     cur_id_counter = 0
+    incrementing_id = 0
+    mult_index = 0
     while (checksum_index < len(block_map) and checksum_index <= block_map_reverse_index):
         cur_char = block_map[checksum_index]
         if cur_char == '.':
             # take number from end of block map
-            print('checksum_index: {}'.format(checksum_index))
-            print('cur_id: {}'.format(cur_id))
-            r = (checksum_index * cur_id)
-            print('r: {}'.format(r))
-            checksum = checksum + r
-            print('checksum in . part: {}'.format(checksum))
+            r = (mult_index * cur_id)            
+            checksum = checksum + r            
             cur_id_counter = cur_id_counter + 1
             block_map_reverse_index = block_map_reverse_index - len(str(cur_id))
-            print('cur_id_counter: {}'.format(cur_id_counter))
-            print('input[0][reverse_index]: {}'.format(input[0][reverse_index]))
+            print('. found; adding {} * {}'.format(checksum_index, cur_id))          
             if cur_id_counter == int(input[0][reverse_index]):
-                print('YOU ARE HERE NOW')
+                print('cur_id_counter reached max ({}) at reverse_index {}'.format(cur_id_counter, reverse_index))
                 cur_id = cur_id - 1
                 cur_id_counter = 0
                 block_map_reverse_index = block_map_reverse_index - int(input[0][reverse_index - 1])
                 reverse_index = reverse_index - 2
+            checksum_index = checksum_index + 1
         else:
-            checksum = checksum + (checksum_index * int(cur_char))
-            print('checksum in else: {}'.format(checksum))
+            # take appropriate number of digits
+            cur_char = block_map[checksum_index:checksum_index + len(str(incrementing_id))]
+            # print('cur_char: {}'.format(cur_char))
+            if (incrementing_id != int(cur_char)):
+                incrementing_id = incrementing_id + 1
+                cur_char = block_map[checksum_index:checksum_index + len(str(incrementing_id))]
 
-        checksum_index = checksum_index + 1
+            print('{} found; adding {} * {}'.format(cur_char, checksum_index, cur_char))
+            checksum = checksum + (mult_index * int(cur_char))
+            checksum_index = checksum_index + len(str(incrementing_id))
+        mult_index = mult_index + 1
     return checksum
 
 def part_b(input):
